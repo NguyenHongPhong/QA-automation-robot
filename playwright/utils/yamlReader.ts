b0e243717201ea5b4a50a4d55a8ac393d79ec57c
NguyenHongPhong <phongnguyenwn123@gmail.com>
2026-03-08 15:28:45 +0700

    feat: edit some configued in CI

--- playwright/utils/yamlReader.ts
+++ playwright/utils/yamlReader.ts
@@ -1,20 +1,9 @@
 import fs from 'fs';
 import path from 'path';
 import YAML from 'yaml';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export function loadYaml(fileName: string) {
-    if (process.env.CI && fileName === 'env_dev') {
-        fileName = `${fileName}.env`;
-    } else if (!process.env.CI && fileName === 'env_dev') {
-        fileName = `${fileName}.yaml`;
-    } else {
-        fileName = `${fileName}.yaml`;
-    }
-    const filePath = path.resolve(process.cwd(), `variables/${fileName}`);
+    const filePath = path.resolve(process.cwd(), `variables/${fileName}.yaml`);
     const file = fs.readFileSync(filePath, 'utf8');
     return YAML.parse(file);
 }
